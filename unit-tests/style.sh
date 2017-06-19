b77b56c6b4c8b754c5ef14cbcf0bf709ce646ff2
Quentin GOINAUD <quentin.goinaud@epitech.eu>
2017-06-19 18:59:50 +0200

    mouli + notifs

--- unit-tests/style.sh
+++ unit-tests/style.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-./unit-tests/norme.py . -malloc -return -comment | grep -v 'login' | grep -v 'header' | grep -v 'pedantic' | grep -v 'Header du Makefile invalide' | grep -v 'Options de debug' > out
+./unit-tests/norme.py . -malloc -return -comment | grep -v 'login' | grep -v 'header' | grep -v 'pedantic' | grep -v 'Header' | grep -v 'Options de debug' > out
 #cat out
 
 
 if [ -s out ]; then
   cat out
+  rm out
   exit 1
 else
   echo -e "No norme error found\nCongrats !"
+  rm out
 fi
-
-rm out
