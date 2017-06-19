b8a5f2085e04c525f9895e2f2bd0835b2c195fbf
Quentin GOINAUD <quentin.goinaud@epitech.eu>
2017-06-19 19:12:55 +0200

    fix -Wall

--- unit-tests/style.sh
+++ unit-tests/style.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./unit-tests/norme.py . -malloc -return -comment | grep -v 'login' | grep -v 'header' | grep -v 'pedantic' | grep -v 'Header' | grep -v 'Options de debug' > out
+./unit-tests/norme.py . -malloc -return -comment | grep -v 'login' | grep -v 'header' | grep -v 'pedantic' | grep -v 'Header' | grep -v 'Options de debug' | grep -v 'Wall' > out
 #cat out
 
 
