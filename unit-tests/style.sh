2350325ba3cea091e296b086fb29134221ccd6a5
Quentin GOINAUD <quentin.goinaud@epitech.eu>
2017-06-20 13:16:50 +0200

    return

--- unit-tests/style.sh
+++ unit-tests/style.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./unit-tests/norme.py src -malloc -return -comment | grep -v 'login' | grep -v 'header' | grep -v 'pedantic' | grep -v 'Header' | grep -v 'Options de debug' | grep -v 'Wall' > out
+./unit-tests/norme.py src -malloc -comment | grep -v 'login' | grep -v 'header' | grep -v 'pedantic' | grep -v 'Header' | grep -v 'Options de debug' | grep -v 'Wall' > out
 #cat out
 
 
