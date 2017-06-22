f98d264112738bed2b5910222bc5bef0eddba9b7
alais_m <martin.alais@epitech.eu>
2017-06-22 11:40:54 +0200

    Take food

--- src/take_object2.c
+++ src/take_object2.c
@@ -5,7 +5,7 @@
 ** Login   <loic1.doyen@epitech.eu@epitech.eu>
 **
 ** Started on  Tue Jun 20 10:03:35 2017 loic1.doyen@epitech.eu
-** Last update Tue Jun 20 17:44:20 2017 Martin Alais
+** Last update Thu Jun 22 11:40:34 2017 Martin Alais
 */
 
 #include "zappy.h"
@@ -34,9 +34,9 @@ void take_food(int id, t_Server *server)
 	tmp = server->list_player;
 	while (tmp->next && tmp->id != id)
 		tmp = tmp->next;
-	if (server->world->map[tmp->pos.x][tmp->pos.y]->thystane >= 1)
+	if (server->world->map[tmp->pos.x][tmp->pos.y]->food >= 1)
 	{
-		server->world->map[tmp->pos.x][tmp->pos.y]->thystane -= 1;
+		server->world->map[tmp->pos.x][tmp->pos.y]->food -= 1;
 		tmp->death_time += 126;
 		printf("player %d eat food !\n", tmp->id);
 		send_message(tmp->fd, "OK\n");
