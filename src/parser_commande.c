2f3ff351ca55926ca0810b28f106a63a506c32da
alais_m <martin.alais@epitech.eu>
2017-06-23 17:12:27 +0200

    unknow cmd

--- src/parser_commande.c
+++ src/parser_commande.c
@@ -5,7 +5,7 @@
 ** Login   <martin.alais@epitech.eu>
 **
 ** Started on  Thu Jun 22 13:44:21 2017 Martin Alais
-** Last update Fri Jun 23 16:26:43 2017 Martin Alais
+** Last update Fri Jun 23 17:12:03 2017 Martin Alais
 */
 
 #include "Graphic.h"
@@ -16,8 +16,8 @@ void command_not_found(int id, t_Server *server)
   t_Player *tmp;
 
   tmp = get_Player(id, server->list_player);
-  tmp->isGraphic == true ? stok_answer(tmp, "suc\n") :
-      stok_answer(tmp, "ko\n");
+  tmp->isGraphic == true ? send_message(tmp->fd, "suc\n") :
+      send_message(tmp->fd, "ko\n");
 }
 
 int get_size_commmande(char *str)
