c1b7f26e02ff1d53b8769d2fbf2b4b397d189057
macdoy <loic1.doyen@epitech.eu>
2017-06-27 10:45:44 +0200

    broadcast update

--- src/command_broadcast.c
+++ src/command_broadcast.c
@@ -5,26 +5,34 @@
 ** Login   <loic1.doyen@epitech.eu@epitech.eu>
 **
 ** Started on  Thu Jun 22 13:36:04 2017 loic1.doyen@epitech.eu
-** Last update Thu Jun 22 13:51:25 2017 loic1.doyen@epitech.eu
+** Last update Thu Jun 22 14:38:27 2017 loic1.doyen@epitech.eu
 */
 
 #include "zappy.h"
 #include "Incantation.h"
 
-int command_broadcast(int id, t_Server *server, char *data)
+int		command_broadcast(int id, t_Server *server, char *data)
 {
-  (void)data;
   t_Player	*tmp;
+  char		*message;
 
   tmp = server->list_player;
-  data = &data[10];
-  strcat(data, "\n");
+  if (strlen(data) < 10)
+    return (1);
+  message = my_malloc(strlen(data));
+  strcpy(message, ", ");
+  strcat(message, &data[10]);
+  strcat(message, "\n");
     while (tmp) {
       if (tmp->id == id)
 	send_message(tmp->fd, "OK\n");
       else
-	send_message(tmp->fd, data);
+	{
+	  send_message(tmp->fd, "message K");
+	  send_message(tmp->fd, message);
+	}
       tmp = tmp->next;
     }
+  free(message);
   return (0);
 }
