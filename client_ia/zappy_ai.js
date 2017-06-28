4c7495ed92c6d1100d1cad7d09f171c57943d204
Quentin GOINAUD <quentin.goinaud@epitech.eu>
2017-06-28 15:39:43 +0200

    no more crashes

--- client_ia/zappy_ai.js
+++ client_ia/zappy_ai.js
@@ -2,30 +2,31 @@
  * Created by goinau_q on 21/06/17.
  */
 
-const stones = [
-	"linemate",
-	"deraumere",
-	"sibur",
-	"mendiane",
-	"phiras",
-	"thystame"
-];
+/* const stones = [
+ "linemate",
+ "deraumere",
+ "sibur",
+ "mendiane",
+ "phiras",
+ "thystame"
+ ]; */
 
-let clients = [];
+const clients = [];
 
 /* Sockets */
-const net    = require('net');
+const net = require('net');
+
 const client = new net.Socket();
 
 /* File system */
-const fs   = require("fs");
+const fs   = require('fs');
 const path = require('path');
 
 /* Colors */
 const chalk = require('chalk');
 
 /* Custom modules */
-const Bot = require("./bot.js");
+const Bot = require('./bot.js');
 
 client.setEncoding('utf8');
 
@@ -34,38 +35,38 @@ client.setEncoding('utf8');
  * @type {any}
  */
 const yargs = require('yargs').options({
-	'behaviour': {
+	behaviour: {
 		alias   : 'b',
 		describe: 'Specify an IA behaviour',
 		default : 'default'
 	},
-	'port'     : {
+	port     : {
 		alias   : 'p',
-		describe: 'port is the port number',
+		describe: 'port is the port number'
 	},
-	'name'     : {
+	name     : {
 		alias   : 'n',
-		describe: 'name is the name of the team',
+		describe: 'name is the name of the team'
 	},
-	'machine'  : {
+	machine  : {
 		alias   : 'h',
 		describe: 'machine is the name of the machine; localhost by default',
 		default : 'localhost'
 	},
-	'browser'  : {
+	browser  : {
 		alias   : 'b',
 		describe: 'specify the port for communication to the browser'
 	}
 }).demandOption(['port', 'name'], 'Please provide both port and name arguments')
 							  .help().argv;
 
-//TODO Check errors !
+// TODO Check errors !
 
 /**
  * Communication with client ---------------------------------------------------
  */
 
-if (yargs.browser !== "default") {
+if (yargs.browser !== 'default') {
 	const app = require('http').createServer(handler);
 	const io  = require('socket.io')(app);
 
@@ -73,7 +74,7 @@ if (yargs.browser !== "default") {
 
 	function handler (req, res) {
 		fs.readFile(__dirname + '/index.html',
-			function (err, data) {
+			(err, data) => {
 				if (err) {
 					res.writeHead(500);
 					return res.end('Error loading index.html');
@@ -84,12 +85,12 @@ if (yargs.browser !== "default") {
 			});
 	}
 
-	io.on('connection', function (socket) {
-		console.log("New client connected");
+	io.on('connection', socket => {
+		console.log('New client connected');
 		clients.push(socket);
 
-		socket.on('msg', function (msg) {
-			console.log("New message from client : " + msg);
+		socket.on('msg', msg => {
+			console.log('New message from client : ' + msg);
 		});
 	});
 }
@@ -99,7 +100,7 @@ if (yargs.browser !== "default") {
  */
 
 /* New bot */
-let bot    = new Bot(yargs.behaviour);
+const bot  = new Bot(yargs.behaviour);
 bot.team   = yargs.name;
 bot.client = client;
 
@@ -107,102 +108,112 @@ bot.client.connect(yargs.port, yargs.machine, () => {
 	console.log('Client connected');
 });
 
-bot.client.on('data', (data) => {
+bot.client.on('data', data => {
 	// When data received, add it to the flux
 	bot.flux += data;
 
-	// if flux is completed (\n), proceed to commands
+	// If flux is completed (\n), proceed to commands
 	if (data[data.length - 1] === '\n') {
-
-		// make all lowercase so we can write all in lowercase
+		// Make all lowercase so we can write all in lowercase
 		bot.flux = bot.flux.toLowerCase();
 
-		// sometimes we receive multiple commands in one request (aka xxx\nxxx\n) so
+		// Sometimes we receive multiple commands in one request (aka xxx\nxxx\n) so
 		// we split \n and remove empty elements
-		let msgs = bot.flux.split("\n").filter(v => v !== '');
+		const msgs = bot.flux.split('\n').filter(v => v !== '');
 
-		// so for each command we receive
-		msgs.forEach((msg) => {
+		// So for each command we receive
+		msgs.forEach(msg => {
 			bot.msg = msg;
 
-			// if browser clients are connected, send it the state of the bot
+			// If browser clients are connected, send it the state of the bot
 			if (clients.length > 0) {
 				clients[0].emit('message', bot.getState());
 			}
 
-			//Wild messages appear
+			// Wild messages appear
 			// TODO refactor
-			if (bot.msg === "welcome")
+			if (bot.msg === 'welcome') {
 				bot.send(bot.team);
-			else if (bot.msg === "ko")
-				bot.send("Look");
-			else if (bot.msg === "dead")
-				console.log(chalk.red("You are dead"));
-			else if (bot.msg === "elevation underway") {
+			} else if (bot.msg === 'ko') {
+				console.log('KO Queue : ' + bot.queue[0]);
+				if (bot.queue[0] !== 'Incantation') {
+					bot.lastCommand = bot.queue.shift();
+					bot.output(bot.lastCommand + ' : ko');
+					bot.send('Look');
+				} else if (bot.incantationStep === 0) {
+					bot.incantationStep = 1;
+				} else if (bot.incantationStep === 1) {
+					bot.queue.shift();
+					bot.incantationStep = 0;
+					bot.goRandomDir();
+				}
+			} else if (bot.msg === 'dead') {
+				bot.dead = true;
+				console.log(chalk.red('You are dead'));
+			} else if (bot.msg === 'elevation underway') {
 				bot.incantating = true;
-				bot.output("Incantation started");
+				bot.output('Incantation started');
 			} else if (bot.msg.match(/current level: (.*)/g)) {
-				bot.level = bot.msg.replace("current level: ", "") * 1;
+				bot.level = Number(bot.msg.replace('current level: ', ''));
 				bot.queue.shift();
 				bot.incantating = false;
 				bot.onIncantation();
 			} else {
-
-				//When no errors if command is XXX
+				// When no errors if command is XXX
 
 				bot.lastCommand = bot.queue[0];
 				if (bot.queue[0] !== bot.team) {
 					bot.lastCommand = bot.queue.shift();
 				}
 
-				console.log(chalk.green("[Receiving] " + bot.lastCommand + ' : ' + JSON.stringify(msg)));
-				//bot.output(bot.queue);
+				console.log(chalk.green('[Receiving] ' + bot.lastCommand + ' : ' + JSON.stringify(msg)));
+				// Bot.output(bot.queue);
 
 				switch (bot.lastCommand) {
 
-					case "Look":
+					case 'Look':
 						bot.onLook();
 						break;
 
-					case "Take":
+					case 'Take':
 						bot.onTake();
 						break;
 
-					case "Fork":
-						bot.output("A new player will be available soon");
+					case 'Fork':
+						bot.output('A new player will be available soon');
 						break;
 
-					case "Forward":
+					case 'Forward':
 						bot.onForward();
 						break;
 
-					case "Right":
+					case 'Right':
 						bot.onRight();
 						break;
 
-					case "Left":
+					case 'Left':
 						bot.onLeft();
 						break;
 
-					case "Set":
+					case 'Set':
 						break;
 
 					case bot.team:
-						if (bot.clientNum === -1)
-							bot.clientNum = bot.flux * 1;
-						else {
-							let size      = bot.flux.split(" ");
-							bot.mapSize.x = size[0] * 1;
-							bot.mapSize.y = size[1] * 1;
+						if (bot.clientNum === -1) {
+							bot.clientNum = Number(bot.flux);
+						} else {
+							const size    = bot.flux.split(' ');
+							bot.mapSize.x = Number(size[0]);
+							bot.mapSize.y = Number(size[1]);
 							bot.queue.shift();
 							bot.onTeam();
 						}
 						break;
 
-					case "Incantation":
+					case 'Incantation':
 						break;
 
-					case "Inventory":
+					case 'Inventory':
 						bot.onInventory();
 						break;
 
@@ -213,15 +224,15 @@ bot.client.on('data', (data) => {
 				// Change direction
 				if (bot.goesUp === bot.mapSize.y) {
 					bot.turning = true;
-					bot.send("Right");
-					bot.send("Forward");
-					bot.send("Left");
+					bot.send('Right');
+					bot.send('Forward');
+					bot.send('Left');
 					bot.goesUp = 0;
 				}
 			}
 		});
 
-		bot.flux = "";
+		bot.flux = '';
 	}
 });
 
@@ -229,22 +240,26 @@ bot.client.on('close', () => {
 	console.log(chalk.red('Connection closed'));
 });
 
-bot.client.on('end', function () {
+bot.client.on('end', () => {
 	console.log(chalk.red('Disconnected from server'));
 });
 
-bot.client.on('error', (err) => {
-	console.log(chalk.red("Error : ", err.code));
+bot.client.on('error', err => {
+	console.log(chalk.red('Error : ', err.code));
 });
 
-setInterval(() => {
+const interval = setInterval(() => {
+	if (bot.dead) {
+		clearInterval(interval);
+		return;
+	}
 	if (bot.inactivity > 3) {
-		bot.send("Look");
+		bot.send('Look');
 		bot.inactivity = 0;
 	}
-	if (bot.queue.length === 0)
+	if (bot.queue.length === 0) {
 		bot.inactivity++;
-	else
+	} else {
 		bot.inactivity = 0;
-
+	}
 }, 1000);
