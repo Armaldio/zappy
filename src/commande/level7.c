a436add4c24afbf5fc5c8272a5137ba282e4d41d
alais_m <martin.alais@epitech.eu>
2017-06-28 21:42:28 +0200

    end level 7

--- src/commande/level7.c
+++ src/commande/level7.c
@@ -5,7 +5,7 @@
 ** Login   <martin.alais@epitech.eu>
 **
 ** Started on  Mon Jun 26 13:31:38 2017 Martin Alais
-** Last update Wed Jun 28 21:29:01 2017 Martin Alais
+** Last update Wed Jun 28 21:40:25 2017 Martin Alais
 */
 
 #include "zappy.h"
@@ -56,13 +56,35 @@ t_Player **get_list_level_7(t_Server *server, t_Player *player)
 	return (tmp2);
 }
 
+bool incan_7bis(t_Server *server, t_Player *player)
+{
+	t_Player **tmp2;
+
+	if (check_nbr_at_level(server, player, 6) == 6)
+	{
+		tmp2 = get_list_level_7(server, player);
+		if (tmp2[0] != NULL && tmp2[1] != NULL &&
+			tmp2[2] != NULL && tmp2[3] != NULL && tmp2[4] != NULL)
+		{
+			player->action->is_leveling = false;
+			tmp2[0]->action->is_leveling = false;
+			tmp2[1]->action->is_leveling = false;
+			tmp2[2]->action->is_leveling = false;
+			tmp2[3]->action->is_leveling = false;
+			tmp2[4]->action->is_leveling = false;
+			complete_struct6(tmp2, player, false);
+			return (false);
+		}
+	}
+	return (true);
+}
+
 void incan_7(t_Server *server, t_Player *player)
 {
 	t_Player **tmp2;
 
 	if (compare_tab(build_tab(server, player), build_tab_7()) &&
-		nbr_case_rdy(server, player) == 6)
-	if (check_nbr_at_level(server, player, 7) >= 6)
+		check_nbr_at_level(server, player, 7) == 6)
 	{
 		tmp2 = get_list_level_7(server, player);
 		if (tmp2[0] != NULL && tmp2[1] != NULL &&
@@ -76,6 +98,12 @@ void incan_7(t_Server *server, t_Player *player)
 			tmp2[4]->action->is_leveling = false;
 			complete_struct6(tmp2, player, true);
 			free(tmp2);
+			return ;
 		}
+		else
+			return (clean_player4(player));
 	}
+	else if (incan_7bis(server, player) == false)
+		return ;
+	clean_player4(player);
 }
