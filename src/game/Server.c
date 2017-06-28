0d8882fd65ede022c405396fdf02051bbe9c6c80
alais_m <martin.alais@epitech.eu>
2017-06-28 13:53:28 +0200

    gitkraken c bugé

--- src/game/Server.c
+++ src/game/Server.c
@@ -5,7 +5,7 @@
 ** Login   <martin.alais@epitech.eu>
 **
 ** Started on  Mon Jun 19 19:21:42 2017 Martin Alais
-** Last update Mon Jun 26 16:26:06 2017 Martin Alais
+** Last update Wed Jun 28 13:43:34 2017 Martin Alais
 */
 
 #include "Server.h"
@@ -26,34 +26,6 @@ void add_to_line(t_Player *tmp, char *data_recv, int a, t_Server *server)
 	{
 		printf("%d: %s", tmp->id, data_recv);
 		add_data_in_line(tmp, data_recv);
-	}
-}
-
-void check_data_player(t_Server *server)
-{
-	int a;
-	char data_recv[4096];
-	struct timeval tv;
-	t_Player *tmp;
-	fd_set rfds;
-
-  end_game(server);
-	tmp = server->list_player;
-	memset(data_recv, '\0', 4096);
-	while (tmp != NULL)
-	{
-		FD_ZERO(&rfds);
-		FD_SET(tmp->fd, &rfds);
-		tv.tv_sec = 0;
-		tv.tv_usec = 300;
-		a = select(tmp->fd + 1, &rfds, NULL, NULL, &tv);
-		if (a != 0 && tmp->is_connected == true)
-		{
-			a = recv(tmp->fd, data_recv, 4095, MSG_DONTWAIT);
-			add_to_line(tmp, data_recv, a, server);
-			memset(data_recv, '\0', 4096);
-		}
-		tmp = tmp->next;
 	}
 }
 
