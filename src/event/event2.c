f0d07dca410aafdcd32cca2b4f1dedfdec6ea742
alais_m <martin.alais@epitech.eu>
2017-06-29 16:17:10 +0200

    a

--- src/event/event2.c
+++ src/event/event2.c
@@ -5,7 +5,7 @@
 ** Login   <martin.alais@epitech.eu>
 **
 ** Started on  Sat Jun 24 16:00:16 2017 Martin Alais
-** Last update Wed Jun 28 13:35:50 2017 Martin Alais
+** Last update Thu Jun 29 16:17:03 2017 Martin Alais
 */
 
 #include "zappy.h"
@@ -72,6 +72,8 @@ void event_enw(t_Server *server, int father, t_Player *player)
 	char buffer2[100];
 
 	egg_fa = get_Player(father, server->list_player);
+	if (egg_fa == NULL)
+		return ;
 	memset(buffer2, '\0', 100);
 	sprintf(buffer2, "enw %d %d %d %d\n", egg_fa->id, player->id,
 	player->pos.x, player->pos.y);
