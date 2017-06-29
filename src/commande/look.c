6a3b0977e21c9ba6325e66c92eddde8387bac016
macdoy <loic1.doyen@epitech.eu>
2017-06-29 15:43:22 +0200

    look corrigé

--- src/commande/look.c
+++ src/commande/look.c
@@ -5,7 +5,7 @@
 ** Login   <loic1.doyen@epitech.eu@epitech.eu>
 **
 ** Started on  Tue Jun 20 11:55:27 2017 loic1.doyen@epitech.eu
-** Last update Thu Jun 29 15:32:47 2017 Martin Alais
+** Last update Thu Jun 29 15:38:25 2017 loic1.doyen@epitech.eu
 */
 
 #include "zappy.h"
@@ -38,7 +38,7 @@ void		t_backward(t_Player *tmp, int i, int y, t_Server *server)
   it = 0;
   while (it < 9)
     {
-      if (it != 2 && it != 3)
+      if (it != 1 && it != 2)
 	{
 	  if (current->tab[it] >= 1)
 	    strcat(message, to_print[it]);
@@ -66,7 +66,7 @@ void		t_forward(t_Player *tmp, int i, int y, t_Server *server)
   it = 0;
   while (it < 9)
     {
-      if (it != 2 && it != 3)
+      if (it != 1 && it != 2)
 	{
 	  if (current->tab[it] >= 1)
 	    strcat(message, to_print[it]);
@@ -89,7 +89,7 @@ void		t_right(t_Player *tmp, int i, int y, t_Server *server)
   it = 0;
   while (it < 9)
     {
-      if (it != 2 && it != 3)
+      if (it != 1 && it != 2)
 	{
 	  if (current->tab[it] >= 1)
 	    strcat(message, to_print[it]);
@@ -117,7 +117,7 @@ void		t_left(t_Player *tmp, int i, int y, t_Server *server)
   it = 0;
   while (it < 9)
     {
-      if (it != 2 && it != 3)
+      if (it != 1 && it != 2)
 	{
 	  if (current->tab[it] >= 1)
 	    strcat(message, to_print[it]);
