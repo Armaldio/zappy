4b8dd9312d6f5b4aedb6641d602f6e1a04bca6cb
alais_m <martin.alais@epitech.eu>
2017-06-29 15:48:21 +0200

    oui non

--- src/commande/level1.c
+++ src/commande/level1.c
@@ -5,7 +5,7 @@
 ** Login   <martin.alais@epitech.eu>
 **
 ** Started on  Mon Jun 26 13:12:47 2017 Martin Alais
-** Last update Thu Jun 29 15:33:23 2017 Martin Alais
+** Last update Thu Jun 29 15:48:05 2017 Martin Alais
 */
 
 #include "zappy.h"
@@ -62,7 +62,7 @@ void end_level1(t_Server *server, t_Player *player)
 			player->level = 2;
 			printf("Player %d reach level 2!\n", player->id);
 	    	event_endI(server, player);
-			stok_answer(player, "Elevation Underway\n");
+			stok_answer(player, "Current level: 2\n");
 		}
 		else
 		{
